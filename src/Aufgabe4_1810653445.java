8c0bf00377295184f7bcd13e0bbb37a692b3437d
Luggist <1810653445@stud.fh-kufstein.ac.at>
2018-11-26 21:09:31 +0100

    Aufgabe4

--- src/Aufgabe4_1810653445.java
+++ src/Aufgabe4_1810653445.java
@@ -2,14 +2,14 @@ public class Aufgabe4_1810653445 {
         public static void main(String[] args){
 
             System.out.println("Do-While-Schleife: ");
-            int zahl1 = 1;
+            int zahl1 = 1; //do while Schleife ist eine Fusßgesteuerte schleife, d.h. zuerst wird der Wert errechnet und dann verglichen
             do {
                 System.out.println("Der Schleifenzähler ist: " + zahl1++);
             } while(zahl1 <= 10);
 
             System.out.println(" ");
 
-            System.out.println("for-Schleife: ");
+            System.out.println("for-Schleife: "); //for und while schleife sind Kopfgesteuerte schleifen, Wert wird zuerst verglichen
             for (int zahl2 = 1; zahl2 <= 10; zahl2++){
                 System.out.println("Der Schleifenzähler ist: " + zahl2);
             }
